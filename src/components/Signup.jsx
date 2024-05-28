d015e58ebe5cc6a55f4bbfc35277480fb19d36bd
Jorge Pérez <80980397+CodenautaJorge@users.noreply.github.com>
2024-05-28 15:49:45 +0200

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -38,9 +38,8 @@ function Signup() {
   };
 
   return (
-    <div className="d-flex m-0 vh-100 justify-content-center align-items-center">
-      <div className="row">
-      <div className="col">
+    <div className="d-flex row m-0 vh-100 justify-content-center align-items-center">
+      
       <form onSubmit={handleSubmit}>
         <div className="form-div">
           <h3 className="text-center text-light fw-bold mb-3">CHAT</h3>
@@ -85,8 +84,7 @@ function Signup() {
           )}
         </div>
       </form>
-    </div>
-    </div>
+   
     </div>
   );
 }
