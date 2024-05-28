b7d393ae8b52abbdf771c855e6bfa794360eaf74
Jorge Pérez <80980397+CodenautaJorge@users.noreply.github.com>
2024-05-28 15:33:16 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,6 +37,7 @@ function Login() {
 
   return (
     <div className="d-flex row m-0 vh-100 justify-content-center align-items-center">
+      <div className="col">
       <form onSubmit={handleSubmit}>
         <div className="form-div">
           <h3 className="text-center text-light fw-bold mb-3">CHAT</h3>
@@ -72,6 +73,7 @@ function Login() {
           )}
         </div>
       </form>
+        </div>
     </div>
   );
 }
