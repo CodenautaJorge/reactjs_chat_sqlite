5be050c7a2de8f7a4f9b9626bb6d7749cbebf1ca
Jorge Pérez <80980397+CodenautaJorge@users.noreply.github.com>
2024-05-28 17:11:38 +0200

    Update LoadingSpinner.jsx

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ const LoadingSpinner = () => {
   return (
     <div className="d-flex justify-content-center align-items-center">
       <div className="spinner-border text-light" role="status">
-        <span className="sr-only">Cargando...</span>
+        
       </div>
     </div>
   );
